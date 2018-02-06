cf205494feaf71e7454bc8b9343aac29508b7e74
Eric Wood <ewood1@underarmour.com>
2018-02-05 18:25:22 -0600

    Initial commit.

---
+++ src/main/java/io/uacf/infra/tracingdemo/TracingdemoApplication.java
@@ -0,0 +1,12 @@
+package io.uacf.infra.tracingdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TracingdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TracingdemoApplication.class, args);
+	}
+}
---
+++ src/test/java/io/uacf/infra/tracingdemo/TracingdemoApplicationTests.java
@@ -0,0 +1,16 @@
+package io.uacf.infra.tracingdemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TracingdemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
